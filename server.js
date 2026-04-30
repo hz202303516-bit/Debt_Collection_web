d9d6c0e190e1bd3e18ada529c861aed63f5d0c8e
hz202303516-bit <hz202303516@wmsu.edu.ph>
2026-04-30 09:59:52 +0800

    Update server.js

--- server.js
+++ server.js
@@ -15,9 +15,7 @@ const pool = require('./database');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+
 
 app.use(cors());
 app.use('/api/auth', authRoutes);
